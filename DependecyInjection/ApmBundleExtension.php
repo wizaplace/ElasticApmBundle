b89d8dffd230bb1333491804dee6817c4cfeb1e9
alex-mercier <alex@wizaplace.com>
2019-12-20 16:42:29 +0100

    load service configuration

---
+++ DependecyInjection/ApmBundleExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Wizacha\ApmBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ApmBundleExtension extends Extension
+{
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../config')
+        );
+        $loader->load('services.yml');
+    }
+}
