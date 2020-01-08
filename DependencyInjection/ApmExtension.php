253feefdd7c2997f257742b3dc5ee049382ce49d
alex-mercier <alex@wizaplace.com>
2020-01-08 16:40:03 +0100

    missing letter

--- DependencyInjection/ApmExtension.php
+++ DependencyInjection/ApmExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 
-class ApmBundleExtension extends Extension
+class ApmExtension extends Extension
 {
     public function load(array $config, ContainerBuilder $container)
     {
