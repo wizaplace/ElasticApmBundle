d6077cc1395f78482bc8c2269e9c10e5bed8ee66
alex-mercier <alex@wizaplace.com>
2020-01-27 16:01:27 +0100

    get proper apm enabled parameter

--- src/DependencyInjection/Compiler/ElasticApmEnginePass.php
+++ src/DependencyInjection/Compiler/ElasticApmEnginePass.php
@@ -7,18 +7,15 @@
 
 namespace Wizacha\ElasticApmBundle\DependencyInjection\Compiler;
 
-use PhilKra\Agent;
-use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 
 
 class ElasticApmEnginePass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
-        $elasticApmEnabled = $container->resolveEnvPlaceholders($container->getParameter('elastic_apm.enabled'));
+        $elasticApmEnabled = $container->resolveEnvPlaceholders($container->getParameter('elastic_apm.enabled'), true);
 
         if (false === $elasticApmEnabled) {
             $container->getDefinition('Wizacha\ElasticApm\Service\AgentService')
