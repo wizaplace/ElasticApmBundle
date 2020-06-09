1c531259ac89ff48d5d19adebf638cdd0c6c4abb
Eric COURTIAL <eric.courtial@wizacha.com>
2020-06-09 11:34:00 +0200

    Improve CI

--- src/DependencyInjection/Compiler/ElasticApmEnginePass.php
+++ src/DependencyInjection/Compiler/ElasticApmEnginePass.php
@@ -1,9 +1,12 @@
 <?php
+
 /**
  * @author      Wizacha DevTeam <dev@wizacha.com>
  * @copyright   Copyright (c) Wizacha
  * @license     Proprietary
  */
+
+declare(strict_types=1);
 
 namespace Wizacha\ElasticApmBundle\DependencyInjection\Compiler;
 
